23a3715c62b0170d2f07f7257df5720aebc92f86
ErwinRomelLee <erwin@mytalentx.com>
2018-12-01 22:25:10 +0900

    etc: /main -> /

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/main', indexRouter);
+app.use('/', indexRouter);
 app.use('/destination', destinationRouter);
 
 // var port = new SerialPort('/dev/ttyACM0',{
