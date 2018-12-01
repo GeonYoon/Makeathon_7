8a65218ba01289ea5274213c2843aa1ab31dc4ff
ErwinRomelLee <erwin@mytalentx.com>
2018-12-02 05:47:23 +0900

    port 수정

--- frontend/src/actions/form.js
+++ frontend/src/actions/form.js
@@ -21,7 +21,7 @@ export const form = (about1, about2, history) => async dispatch => {
 export const updateColor = (value) => async dispatch => {
     const sending = {"destination" : value.name}
     // const res = await axios.post('http://192.168.0.103:6508/destination/',sending)
-    const socket = socketIOClient("http://127.0.0.1:6508");
+    const socket = socketIOClient("http://192.168.0.103:6508");
     socket.emit("message",{"destination" : value.name})
 
     dispatch({ type : UPDATE_COLOR, payload : value});
